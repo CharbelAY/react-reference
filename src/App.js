de37bc2bcd47aaabe67e7b08b739bcf4577a1ec0
Charbel Abou Younes <charbel@abouyounes.com>
2022-12-17 10:29:38 +0200

    0008 format code

--- src/App.js
+++ src/App.js
@@ -17,36 +17,39 @@ class App extends React.Component {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
             .then((responseJson) => {
-                const monsters =[];
+                const monsters = [];
                 for (let key in responseJson) {
                     monsters.push({
                         id: responseJson[key].id,
                         name: responseJson[key].name
                     })
                 }
-                this.setState(() => { return {monsters: monsters}}, () => console.log(monsters));
-        })
-        }
+                this.setState(() => {
+                    return {monsters: monsters}
+                }, () => console.log(monsters));
+            })
+    }
 
     render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-              {
-                  // when looping and rendering items they need to have a unique key to help react identify
-                  // them when manipulating the items
-                  // and changing their properties
-                  <>
-                  <h1>{this.state.name}</h1>
-                      {this.state.monsters.map(monster =>
-                          <h1 key={monster.id}>{monster.name}</h1>
-                      )}
-                  </>
-              }
-          </header>
-        </div>
-    );
-  }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {
+                        // when looping and rendering items they need to have a unique key to help react identify
+                        // them when manipulating the items
+                        // and changing their properties
+                        <>
+                            <input/>
+                            <h1>{this.state.name}</h1>
+                            {this.state.monsters.map(monster =>
+                                <h1 key={monster.id}>{monster.name}</h1>
+                            )}
+                        </>
+                    }
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
